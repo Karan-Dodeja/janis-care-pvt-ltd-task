9cb29d42763d6c9b2ea2709be182bf9892a96b64
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-06-20 18:49:46 +0530

    Banner

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,27 +3,31 @@ import PreLoader from "../Images/Winter.gif";
 import "./components.css";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import Img1 from "../Images/mask-1.jpg";
+import Img2 from "../Images/mask-2.jpg";
+import Img3 from "../Images/Iodine1.jpg";
+import Img4 from "../Images/mask-3.jpg";
+import Img5 from "../Images/mask-4.jpg";
 const Home = () => {
   return (
     <>
-      <Header />
       <div className="Home">
+        <Header />
         <div className="home-banner">
-          <div className="banner-img-1">
-            <img src="" alt="" />
+          <div>
+            <img src={Img1} alt="" className="banner-img-1" />
           </div>
-          <div className="banner-img-2">
-            <img src="" alt="" />
+          <div>
+            <img src={Img2} alt="" className="banner-img-2" />
           </div>
-          <div className="banner-img-3">
-            <img src="" alt="" />
+          <div>
+            <img src={Img3} alt="" className="banner-img-3" />
           </div>
-          <div className="banner-img-4">
-            <img src="" alt="" />
+          <div>
+            <img src={Img4} alt="" className="banner-img-4" />
           </div>
-          <div className="banner-img-5">
-            <img src="" alt="" />
+          <div>
+            <img src={Img5} alt="" className="banner-img-5" />
           </div>
         </div>
         <div className="home-counter">
@@ -31,8 +35,8 @@ const Home = () => {
           <div className="counter-2"></div>
           <div className="counter-3"></div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
