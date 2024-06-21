8ed268459dbe37acf0a3aaa75257fec26c15b43b
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-06-21 06:13:09 +0530

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import PreLoader from "../Images/Winter.gif";
-
+import Logo from "../Images/jenis-care-logo-1.png";
+import { Link } from "react-router-dom";
 import "./components.css";
+
 const Footer = () => {
   return (
     <div className="Footer">
       <div className="footer-logo">
-        <img src="" alt="" />
+        <img src={Logo} alt="" className="footer-logo" />
       </div>
       <div className="footer-tabs">
-        <img src="" alt="" />
+        <div>
+          <Link className="footer-tabs" to="/">
+            Home
+          </Link>
+          <Link to="/" className="footer-tabs">
+            About Us
+          </Link>
+          <Link to="/" className="footer-tabs">
+            Products
+          </Link>
+          <Link to="/" className="footer-tabs">
+            Contact Us
+          </Link>
+        </div>
       </div>
     </div>
   );
